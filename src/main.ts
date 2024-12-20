2d6943d786360315dcff2e25aedfe9396c49a68f
blazery <j.roosenmaallen@hotmail.com>
2024-12-20 22:49:39 +0100

    ERROR - removed error from project

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import './style.css'
+import { IpcRenderer } from 'electron';
+
+// fixes issues of window.ipcRenderer not being available
+declare global {
+  interface Window {
+    ipcRenderer: IpcRenderer;
+  }
+}
 
 createApp(App)
   .mount('#app')
